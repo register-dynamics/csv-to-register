7ec6c3ac2d548126a09ec94fa45b0025c091c34b
timwis <tim@timwis.com>
2019-09-30 12:44:56 +0100

    Support metadata option

--- README.md
+++ README.md
@@ -7,9 +7,36 @@ With Python 3 and [Pipenv][pipenv] installed:
 
 1. Clone this repository and run `pipenv install` within it.
 2. Run `pipenv shell` to activate a local environment shell.
-3. Run the script using `python bin/cli.py <CSV_FILE> --key-field <FIELD_NAME> > output.rsf`
+3. Run the script using `python bin/cli.py`
 
-You can pipe the CSV file to it by passing `-` as the file name argument, e.g. `cat file.csv | python bin/cli.py -k id > output.rsf`
+```
+Usage: cli.py [OPTIONS] CSV_FILE
+
+  Convert CSV file to RSF format
+
+Options:
+  -k, --key-field TEXT     Field to use as key  [required]
+  -m, --metadata FILENAME  JSON-formatted metadata
+  --help                   Show this message and exit.
+```
+
+Basic example:
+
+```
+python bin/cli.py --key id data.csv > output.rsf
+```
+
+You can pipe the CSV file to it by passing `-` as the file name argument.
+
+```
+cat file.csv | python bin/cli.py -k id - > output.rsf
+```
+
+You can include metadata in the output by passing a JSON file to the `--metadata` flag. The JSON file should be an object where the top-level keys are used as entry keys. 
+
+```
+python bin/cli.py -k id -m metadata.json data.csv > output.rsf
+```
 
 [orc]: https://github.com/register-dynamics/orc
 [pipenv]: https://pipenv.readthedocs.io/en/latest/
--- bin/cli.py
+++ bin/cli.py
@@ -9,26 +9,44 @@ from tqdm import tqdm # progress bar
 @click.command()
 @click.argument('csv_file', type=click.File())
 @click.option('-k', '--key-field', required=True, help='Field to use as key')
-@click.option('--region', default='user')
-def csv_to_rsf(csv_file, key_field, region):
+@click.option('-m', '--metadata', 'metadata_file', type=click.File(), help='JSON-formatted metadata')
+def csv_to_rsf(csv_file, key_field, metadata_file):
   """Convert CSV file to RSF format"""
   print('assert-root-hash\t{}'.format(hash('')))
 
   timestamp = get_timestamp()
+
+  if metadata_file:
+    metadata = json.load(metadata_file)
+    for key, value in metadata.items():
+      [add_item, append_entry] = format_rsf_item(region='system', key=key,
+                                    timestamp=timestamp, value=value)
+      print(add_item)
+      print(append_entry)
+
   reader = csv.DictReader(csv_file)
   for row in tqdm(reader):
     key = row.get(key_field) # TODO: Output rows missing keys to stderr
-    row_json = json.dumps(row)
-    row_hash = hash(row_json)
-    print('add-item\t{}'.format(row_json))
-    print('append-entry\t{}\t{}\t{}\t{}'.format(
-          region, key, timestamp, row_hash))
+    [add_item, append_entry] = format_rsf_item(region='user', key=key,
+                                timestamp=timestamp, value=row)
+    print(add_item)
+    print(append_entry)
 
 def get_root_hash():
   return hash('')
 
 def get_timestamp():
   return datetime.utcnow().isoformat(timespec='seconds') + 'Z'
+
+def format_rsf_item(region, key, timestamp, value):
+  value_json = json.dumps(value)
+  value_hash = hash(value_json)
+
+  return [
+    'add-item\t{}'.format(value_json),
+    'append-entry\t{}\t{}\t{}\t{}'.format(
+      region, key, timestamp, value_hash),
+  ]
 
 def hash(payload):
   return 'sha-256:' + sha256(payload.encode('utf-8')).hexdigest()
